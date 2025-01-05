9758cac0f14e465aeb01db7875a51cf4ee028790
명길식 <myeong-gilsig@MBook.local>
2025-01-05 18:57:57 +0900

    agp 8.7.3 to 8.3.0

--- app/src/main/java/com/example/kotlin06_todolist/MainActivity.kt
+++ app/src/main/java/com/example/kotlin06_todolist/MainActivity.kt
@@ -2,8 +2,12 @@ package com.example.kotlin06_todolist
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.kotlin06_todolist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
